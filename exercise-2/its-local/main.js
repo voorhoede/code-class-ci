3576894ca6f4422cefa6ab328d588932ecbf5fd8
Zowie <zowie@zowievangeest.nl>
2021-02-05 09:54:06 +0100

    zowie-exercise-2

--- exercise-2/its-local/main.js
+++ exercise-2/its-local/main.js
@@ -1,6 +1,6 @@
-import { debug } from '@actions/core';
+import {debug, getInput} from '@actions/core';
 
 debug(`GitHub Event Name: ${process.env.GITHUB_EVENT_NAME}`);
 
-console.info(`╰( ⁰ ਊ ⁰ )━☆ﾟ.*･｡ﾟ `);
+console.info(`╰( ⁰ ਊ ⁰ )━☆ﾟ.*･｡ﾟ ${getInput('message')}`);
 //       message input here ${} ^
