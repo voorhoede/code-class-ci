021e8973a4f3bfb1e1f8ba548f5f433bdd218e83
Celine Kurpershoek <celinekurpershoek@gmail.com>
2021-02-05 09:56:22 +0100

    Test logging message with action

--- exercise-2/its-local/main.js
+++ exercise-2/its-local/main.js
@@ -1,6 +1,6 @@
-import { debug } from '@actions/core';
+import { debug, getInput } from '@actions/core';
 
 debug(`GitHub Event Name: ${process.env.GITHUB_EVENT_NAME}`);
+const myInput = getInput('message', { required: true });
+console.info(`╰( ⁰ ਊ ⁰ )━☆ﾟ.*･｡ﾟ ${myInput}`);
 
-console.info(`╰( ⁰ ਊ ⁰ )━☆ﾟ.*･｡ﾟ `);
-//       message input here ${} ^
