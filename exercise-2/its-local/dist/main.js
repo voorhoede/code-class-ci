63868acaabbcf5a41a12436f3eaeb791c03e9ff3
Bas de Greeuw <basdegreeuw@voorhoede.nl>
2021-02-05 09:57:58 +0100

    lol wel builden

--- exercise-2/its-local/dist/main.js
+++ exercise-2/its-local/dist/main.js
@@ -397,7 +397,8 @@ exports.getState = getState;
 
 });
 
+const message = core.getInput('message');
 core.debug(`GitHub Event Name: ${process.env.GITHUB_EVENT_NAME}`);
 
-console.info(`╰( ⁰ ਊ ⁰ )━☆ﾟ.*･｡ﾟ `);
+console.info(`╰( ⁰ ਊ ⁰ )━☆ﾟ.*･｡ﾟ ${message}`);
 //       message input here ${} ^
