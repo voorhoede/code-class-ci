891392ebb41390f6f0920453f646fc30b5d939e6
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2021-02-05 09:53:05 +0100

    show message

--- exercise-2/its-local/main.js
+++ exercise-2/its-local/main.js
@@ -1,6 +1,5 @@
-import { debug } from '@actions/core';
+import { debug, getInput } from "@actions/core";
 
 debug(`GitHub Event Name: ${process.env.GITHUB_EVENT_NAME}`);
 
-console.info(`╰( ⁰ ਊ ⁰ )━☆ﾟ.*･｡ﾟ `);
-//       message input here ${} ^
+console.info(`╰( ⁰ ਊ ⁰ )━☆ﾟ.*･｡ﾟ `, getInput("message"));
