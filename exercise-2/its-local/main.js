26e0af8328c6c178134c75d1b7b4cfa9c2a2698a
Frank Warnaar <frank@voorhoede.nl>
2021-02-05 09:53:15 +0100

    frank exercise 2

--- exercise-2/its-local/main.js
+++ exercise-2/its-local/main.js
@@ -1,6 +1,6 @@
-import { debug } from '@actions/core';
+import { debug, getInput } from '@actions/core';
 
 debug(`GitHub Event Name: ${process.env.GITHUB_EVENT_NAME}`);
 
-console.info(`╰( ⁰ ਊ ⁰ )━☆ﾟ.*･｡ﾟ `);
 //       message input here ${} ^
+console.info(`╰( ⁰ ਊ ⁰ )━☆ﾟ.*･｡ﾟ `, getInput('message'));
