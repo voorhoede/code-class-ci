ab40e94186a09644773314059697378256863e03
Bas de Greeuw <basdegreeuw@voorhoede.nl>
2021-02-05 09:55:38 +0100

    added message

--- exercise-2/its-local/main.js
+++ exercise-2/its-local/main.js
@@ -1,6 +1,6 @@
-import { debug } from '@actions/core';
-
+import { debug, getInput } from '@actions/core';
+const message = getInput('message');
 debug(`GitHub Event Name: ${process.env.GITHUB_EVENT_NAME}`);
 
-console.info(`╰( ⁰ ਊ ⁰ )━☆ﾟ.*･｡ﾟ `);
+console.info(`╰( ⁰ ਊ ⁰ )━☆ﾟ.*･｡ﾟ ${message}`);
 //       message input here ${} ^
